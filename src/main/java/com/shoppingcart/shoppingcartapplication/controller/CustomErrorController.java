11d0c40529736212f7e8e350cd08ea24bcbe48e5
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-07-27 14:52:18 +0530

    added custom error pages

---
+++ src/main/java/com/shoppingcart/shoppingcartapplication/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package com.shoppingcart.shoppingcartapplication.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/error")
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping
+    public void handleError(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("error.html").forward(request,response);
+    }
+}
